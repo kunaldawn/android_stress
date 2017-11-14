69aa090bc6793ce92e39e57ccaf6b7317e484086
Kunal Dawn <kunal.dawn@gmail.com>
2017-11-14 11:46:33 +0530

    added PIC support

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,5 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := stress
 LOCAL_SRC_FILES := stress.c
 LOCAL_CFLAGS += -DPACKAGE=\"stress\" -DVERSION=\"1.0.4\"
+LOCAL_CFLAGS += -pie -fPIE
+LOCAL_LDFLAGS += -pie -fPIE
 
 include $(BUILD_EXECUTABLE)
